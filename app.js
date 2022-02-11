5d706beb08512fac4d073ea977ee87e117d805a1
mercado-carlos <carlos-roberto.mercado@atos.net>
2022-02-11 14:10:02 -0600

    Tutorial #4 - The Query Selector

--- app.js
+++ app.js
@@ -11,3 +11,16 @@ var lis = document.getElementsByTagName('li');
 Array.from(titles).forEach((title) => {
     console.log(title);
 });
+
+/* The Query Selector */
+const wrap = document.querySelector('#wrapper');
+const wmf = document.querySelector('#book-list li:nth-child(2) .name');
+var books = document.querySelector('#book-list li .name');
+
+console.log(books);
+
+books = document.querySelectorAll('#book-list li .name');
+
+Array.from(books).forEach((book) => {
+    console.log(book);
+});
