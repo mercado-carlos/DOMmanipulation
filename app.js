58f5b8b963666bff48912eadbe5f765e7d6833fb
mercado-carlos <carlos-roberto.mercado@atos.net>
2022-02-11 13:51:51 -0600

    Tutorial #3 - Get Elements By Class or Tag

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-var search = document.getElementById('search-books');
-var bookList = document.getElementById('book-list');
+/* Get Element By ID */
+const search = document.getElementById('search-books');
+const bookList = document.getElementById('book-list');
 
 console.log(search, bookList);
+
+/* Get Element By Class or Tag */
+var titles = document.getElementsByClassName('title');
+var lis = document.getElementsByTagName('li');
+
+Array.from(titles).forEach((title) => {
+    console.log(title);
+});
